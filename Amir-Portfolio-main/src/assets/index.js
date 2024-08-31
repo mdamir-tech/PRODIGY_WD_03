ba088158708cd5be4a9c42e739a8baf945ad826d
mdamir-tech <141801598+mdamir-tech@users.noreply.github.com>
2024-08-31 10:56:32 +0530

    Initial commit

---
+++ Amir-Portfolio-main/src/assets/index.js
@@ -0,0 +1,27 @@
+import logo from "./images/logo.png";
+import bannerImg from "./images/bannerImg.png";
+
+import projectThree from "./images/projects/projectThree.jpeg";
+import testimonialOne from "./images/testmonial/testimonialOne.png";
+import testimonialTwo from "./images/testmonial/testimonialTwo.png";
+import quote from "./images/testmonial/quote.png";
+import contactImg from "./images/contact/contactImg.png";
+import projectFor from "./images/projects/projectFor.png";
+import projectFive from "./images/projects/projectFive.png";
+import profile from "./images/testmonial/profile.jpg"
+import projectSeven from "./images/projects/projectSeven.jpg";
+import amir from "./images/amir.jpg";
+export {
+  amir,
+  logo,
+  bannerImg,
+  projectThree,
+  testimonialOne,
+  projectFive,
+  projectSeven,
+  testimonialTwo,
+  quote,
+  contactImg,
+  profile,
+  projectFor,
+};
